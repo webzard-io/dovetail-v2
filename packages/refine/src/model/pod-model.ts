8c374a1b5bac51dba69f52324669ae31c11ef860
Yanzhen Yu <yanzhen@smartx.com>
2023-10-09 16:06:53 +0800

    improve model layer types

--- packages/refine/src/model/pod-model.ts
+++ packages/refine/src/model/pod-model.ts
@@ -1,18 +1,25 @@
+import type { Pod } from 'kubernetes-types/core/v1';
 import { shortenedImage } from '../utils/string';
-import { ResourceModel } from './resource-model';
+import { Resource, ResourceModel } from './resource-model';
 
 export class PodModel extends ResourceModel {
+  constructor(public data: Pod & Resource) {
+    super(data);
+  }
+
   get nodeName() {
-    return this.data.spec.nodeName;
+    return this.data.spec?.nodeName || '';
   }
   get imageNames() {
-    return this.data.spec.containers.map((container: any) =>
-      shortenedImage(container.image)
+    return (
+      this.data.spec?.containers.map(container =>
+        shortenedImage(container.image || '')
+      ) || []
     );
   }
 
   get restartCount() {
-    if (this.data.status.containerStatuses) {
+    if (this.data.status?.containerStatuses) {
       return this.data.status?.containerStatuses[0].restartCount || 0;
     }
     return 0;
