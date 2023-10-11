1972f807098c30e29e66aec88e2fbb6ed6d4fef8
Yanzhen Yu <yanzhen@smartx.com>
2023-10-11 14:19:45 +0800

    add daemonset CRUD views

---
+++ packages/refine/src/pages/daemonsets/show/index.tsx
@@ -0,0 +1,28 @@
+import { IResourceComponentsProps } from '@refinedev/core';
+import { DaemonSet } from 'kubernetes-types/apps/v1';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { PageShow } from '../../../components/PageShow';
+import {
+  ConditionsField,
+  ImageField,
+  PodsField,
+  ReplicaField,
+} from '../../../components/ShowContent/fields';
+import { WorkloadModel } from '../../../model';
+import { WithId } from '../../../types';
+
+export const DaemonSetShow: React.FC<IResourceComponentsProps> = () => {
+  const { i18n } = useTranslation();
+
+  return (
+    <PageShow<WithId<DaemonSet>, WorkloadModel>
+      fieldGroups={[
+        [],
+        [ImageField(i18n), ReplicaField(i18n)],
+        [PodsField(i18n), ConditionsField(i18n)],
+      ]}
+      formatter={d => new WorkloadModel(d)}
+    />
+  );
+};
