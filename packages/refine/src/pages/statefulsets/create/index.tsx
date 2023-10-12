e1fa75f5174336429ee703c6e4a3f72721e8e0a4
Yanzhen Yu <yanzhen@smartx.com>
2023-10-12 22:51:33 +0800

    fix some runtime errors

---
+++ packages/refine/src/pages/statefulsets/create/index.tsx
@@ -0,0 +1,35 @@
+import { FormProps } from 'antd/lib/form';
+import React from 'react';
+import YamlForm from 'src/components/YamlForm';
+import { BASE_INIT_VALUE } from 'src/constants/k8s';
+
+export const StatefulSetForm: React.FC<FormProps> = () => {
+  return (
+    <YamlForm
+      initialValues={{
+        apiVersion: 'apps/v1',
+        kind: 'StatefulSet',
+        ...BASE_INIT_VALUE,
+        spec: {
+          replicas: 1,
+          selector: {
+            matchLabels: {},
+          },
+          template: {
+            metadata: {
+              labels: {},
+            },
+            spec: {
+              containers: [
+                {
+                  name: '',
+                  image: '',
+                },
+              ],
+            },
+          },
+        },
+      }}
+    />
+  );
+};
