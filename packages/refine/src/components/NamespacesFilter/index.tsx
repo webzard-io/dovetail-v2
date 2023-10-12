06dd5d79b166f941aef42c23a07c5393471c1fed
Yanzhen Yu <yanzhen@smartx.com>
2023-10-12 12:36:33 +0800

    keep ns filter in local storage

--- packages/refine/src/components/NamespacesFilter/index.tsx
+++ packages/refine/src/components/NamespacesFilter/index.tsx
@@ -1,10 +1,19 @@
 import { useUIKit } from '@cloudtower/eagle';
-import { useGo, useList, useParsed } from '@refinedev/core';
+import { useList } from '@refinedev/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useLocalStorage } from 'usehooks-ts';
 
 export const NS_STORE_KEY = 'namespace-filter';
 export const ALL_NS = '_all';
+
+export const useNamespacesFilter = () => {
+  const [value] = useLocalStorage(NS_STORE_KEY, ALL_NS);
+
+  return {
+    value,
+  };
+};
 
 export const NamespacesFilter: React.FC = () => {
   const kit = useUIKit();
@@ -17,23 +26,14 @@ export const NamespacesFilter: React.FC = () => {
     },
   });
 
-  const parsed = useParsed();
-  const go = useGo();
-  const value = parsed.params?.[NS_STORE_KEY] || ALL_NS;
+  const [value, setValue] = useLocalStorage(NS_STORE_KEY, ALL_NS);
 
   return (
     <kit.select
       input={{
         value,
         onChange(value) {
-          go({
-            query: {
-              [NS_STORE_KEY]: value,
-            },
-            options: {
-              keepQuery: true,
-            },
-          });
+          setValue(value as string);
         },
       }}
     >
