670e2f160755eb0aea2e46240fb9f3bfba807e81
Yanzhen Yu <yanzhen@smartx.com>
2023-10-16 10:57:19 +0800

    improve workload form init values

--- packages/refine/src/pages/daemonsets/create/index.tsx
+++ packages/refine/src/pages/daemonsets/create/index.tsx
@@ -1,35 +1,8 @@
 import { FormProps } from 'antd/lib/form';
 import React from 'react';
 import YamlForm from 'src/components/YamlForm';
-import { BASE_INIT_VALUE } from 'src/constants/k8s';
+import { DAEMONSET_INIT_VALUE } from 'src/constants/k8s';
 
 export const DaemonSetForm: React.FC<FormProps> = () => {
-  return (
-    <YamlForm
-      initialValues={{
-        apiVersion: 'apps/v1',
-        kind: 'DaemonSet',
-        ...BASE_INIT_VALUE,
-        spec: {
-          replicas: 1,
-          selector: {
-            matchLabels: {},
-          },
-          template: {
-            metadata: {
-              labels: {},
-            },
-            spec: {
-              containers: [
-                {
-                  name: '',
-                  image: '',
-                },
-              ],
-            },
-          },
-        },
-      }}
-    />
-  );
+  return <YamlForm initialValues={DAEMONSET_INIT_VALUE} />;
 };
--- packages/refine/src/pages/deployments/create/index.tsx
+++ packages/refine/src/pages/deployments/create/index.tsx
@@ -1,35 +1,8 @@
 import { FormProps } from 'antd/lib/form';
 import React from 'react';
 import YamlForm from 'src/components/YamlForm';
-import { BASE_INIT_VALUE } from 'src/constants/k8s';
+import { DEPLOYMENT_INIT_VALUE } from 'src/constants/k8s';
 
 export const DeploymentForm: React.FC<FormProps> = () => {
-  return (
-    <YamlForm
-      initialValues={{
-        apiVersion: 'apps/v1',
-        kind: 'Deployment',
-        ...BASE_INIT_VALUE,
-        spec: {
-          replicas: 1,
-          selector: {
-            matchLabels: {},
-          },
-          template: {
-            metadata: {
-              labels: {},
-            },
-            spec: {
-              containers: [
-                {
-                  name: '',
-                  image: '',
-                },
-              ],
-            },
-          },
-        },
-      }}
-    />
-  );
+  return <YamlForm initialValues={DEPLOYMENT_INIT_VALUE} />;
 };
