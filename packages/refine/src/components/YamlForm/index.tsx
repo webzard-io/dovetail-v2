249e49ca8e89b68a29b200f3f7e3f94a4daf49f6
Yanzhen Yu <yanzhen@smartx.com>
2023-12-10 12:59:36 +0800

    temp skip fetch schema

--- packages/refine/src/components/YamlForm/index.tsx
+++ packages/refine/src/components/YamlForm/index.tsx
@@ -33,45 +33,41 @@ function YamlForm(props: YamlFormProps) {
         initialValues={formProps.initialValues ?? props.initialValues ?? BASE_INIT_VALUE}
         layout="horizontal"
       >
-        {editorProps.schema ? (
-          <>
-            <kit.form.Item style={{ flex: 1 }}>
-              <YamlEditorComponent
-                {...editorProps}
-                className={EditorStyle}
-                schema={editorProps.schema}
-                collapsable={false}
+        <>
+          <kit.form.Item style={{ flex: 1 }}>
+            <YamlEditorComponent
+              {...editorProps}
+              className={EditorStyle}
+              schema={editorProps.schema}
+              collapsable={false}
+            />
+          </kit.form.Item>
+          <kit.form.Item>
+            {mutationResult.error && (
+              <kit.alert
+                message={
+                  errorResponseBody ? (
+                    <ul>
+                      {responseErrors.map((error, index) => (
+                        <li key={error}>
+                          {responseErrors.length > 1 ? index + 1 + '. ' : ''}
+                          {error}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    mutationResult.error.message
+                  )
+                }
+                type="error"
+                style={{ marginTop: 16 }}
               />
-            </kit.form.Item>
-            <kit.form.Item>
-              {mutationResult.error && (
-                <kit.alert
-                  message={
-                    errorResponseBody ? (
-                      <ul>
-                        {responseErrors.map((error, index) => (
-                          <li key={error}>
-                            {responseErrors.length > 1 ? index + 1 + '. ' : ''}
-                            {error}
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      mutationResult.error.message
-                    )
-                  }
-                  type="error"
-                  style={{ marginTop: 16 }}
-                />
-              )}
-              <kit.button {...saveButtonProps} type="primary" style={{ marginTop: 16 }}>
-                {t('save')}
-              </kit.button>
-            </kit.form.Item>
-          </>
-        ) : (
-          <kit.loading />
-        )}
+            )}
+            <kit.button {...saveButtonProps} type="primary" style={{ marginTop: 16 }}>
+              {t('save')}
+            </kit.button>
+          </kit.form.Item>
+        </>
       </kit.form>
     </FormLayout>
   );
