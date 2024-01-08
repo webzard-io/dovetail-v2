dc3665f84da83e6af6457b6bebf3a7c2ec82ed10
Bowen Tan <bowen.tan@smartx.com>
2024-01-08 17:07:39 +0800

    fix WorkloadModel scale and redeploy bug

--- packages/refine/src/models/workload-model.ts
+++ packages/refine/src/models/workload-model.ts
@@ -1,23 +1,18 @@
- import { GlobalStore, Unstructured } from 'k8s-api-provider';
-import type {
-  DaemonSet,
-  Deployment,
-  StatefulSet,
-} from 'kubernetes-types/apps/v1';
+import { GlobalStore, Unstructured } from 'k8s-api-provider';
+import type { DaemonSet, Deployment, StatefulSet } from 'kubernetes-types/apps/v1';
 import { PodList } from 'kubernetes-types/core/v1';
 import { cloneDeep, get, set, sumBy } from 'lodash';
+import { TIMESTAMP_LABEL } from '../constants';
 import { matchSelector } from '../utils/match-selector';
 import { PodModel } from './pod-model';
 import { WorkloadBaseModel } from './workload-base-model';
-import { TIMESTAMP_LABEL } from '../constants';
 
-type WorkloadTypes = Required<Deployment | StatefulSet | DaemonSet> &
-  Unstructured;
+type WorkloadTypes = Required<Deployment | StatefulSet | DaemonSet> & Unstructured;
 
 export class WorkloadModel extends WorkloadBaseModel {
   public restarts = 0;
-  declare public spec?: WorkloadTypes['spec'];
-  declare public status?: WorkloadTypes['status'];
+  public declare spec?: WorkloadTypes['spec'];
+  public declare status?: WorkloadTypes['status'];
 
   constructor(
     public _rawYaml: WorkloadTypes,
@@ -31,35 +26,35 @@ export class WorkloadModel extends WorkloadBaseModel {
   }
 
   private async getRestarts() {
-    const pods = await this._globalStore.get('pods', {
+    const pods = (await this._globalStore.get('pods', {
       resourceBasePath: '/api/v1',
       kind: 'Pod',
-    }) as PodList;
+    })) as PodList;
     const myPods = pods.items.filter(p =>
-      this.spec?.selector
-        ? matchSelector(p as PodModel, this.spec?.selector)
-        : false
+      this.spec?.selector ? matchSelector(p as PodModel, this.spec?.selector) : false
     );
     const result = sumBy(myPods, 'restartCount');
     this.restarts = result;
   }
 
   redeploy(): WorkloadTypes {
-    const newOne = cloneDeep(this._rawYaml);
+    const rawYaml = this._globalStore.restoreItem(this);
+    const newOne = cloneDeep(rawYaml);
     const path = 'spec.template.metadata.annotations';
     const annotations = get(newOne, path, {});
     set(newOne, path, {
       ...annotations,
       [TIMESTAMP_LABEL]: new Date().toISOString().replace(/\.\d+Z$/, 'Z'),
     });
-    return newOne;
+    return newOne as WorkloadTypes;
   }
 
   scale(value: number): WorkloadTypes {
-    const newOne = cloneDeep(this._rawYaml);
+    const rawYaml = this._globalStore.restoreItem(this);
+    const newOne = cloneDeep(rawYaml);
     if (newOne.kind === 'Deployment' || newOne.kind === 'StatefulSet') {
       set(newOne, 'spec.replicas', value);
     }
-    return newOne;
+    return newOne as WorkloadTypes;
   }
 }
