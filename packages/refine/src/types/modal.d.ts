154b84257ac75a62f54ce8261450eb23e39d20f1
MrWindlike <MrWindlike@gmail.com>
2024-02-06 11:51:17 +0800

    feat: use modal to edit replicas

--- packages/refine/src/types/modal.d.ts
+++ packages/refine/src/types/modal.d.ts
@@ -1,8 +1,10 @@
 import { ModalType } from '@cloudtower/eagle';
+import { EditFieldModalProps } from 'src/components/EditField';
 import { FormModalProps } from 'src/components/FormModal';
 
 type ModalProps = {
   FormModal: FormModalProps;
+  EditFieldModal: EditFieldModalProps;
 };
 
 declare module '@cloudtower/eagle' {
