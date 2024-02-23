aae309d363b3b18230cc2453f73030772e550de4
Bowen Tan <bowen.tan@smartx.com>
2024-02-23 15:44:52 +0800

    Creation time show time period

--- packages/refine/src/components/Time/index.tsx
+++ packages/refine/src/components/Time/index.tsx
@@ -1,40 +1,40 @@
 // TODO: use ui-kit
+import { Tooltip } from '@cloudtower/eagle';
 import dayjs from 'dayjs';
-import React from 'react';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+dayjs.extend(relativeTime);
 
 const Time: React.FunctionComponent<{
   className?: string;
   date?: string | number | Date | null;
-  dateTemplate?: string | null;
-  timeTemplate?: string | null;
-  plainText?: boolean;
+  dateTemplate?: string;
+  timeTemplate?: string;
 }> = props => {
-  const {
-    className,
-    date,
-    dateTemplate = 'YYYY-MM-DD',
-    timeTemplate = 'HH:mm',
-    plainText,
-  } = props;
+  const { className, date, dateTemplate = 'YYYY-MM-DD', timeTemplate = 'HH:mm' } = props;
+  const { i18n } = useTranslation();
+  const [text, setText] = useState(dayjs(date).fromNow());
+
+  useEffect(() => {
+    const onChange = () => {
+      setText(dayjs(date).fromNow());
+    };
+
+    i18n.on('languageChanged', onChange);
+
+    return () => i18n.off('languageChanged', onChange);
+  }, [date, i18n]);
+
   if (!date) return <>-</>;
+
   const time = dayjs(date);
-  if (plainText) {
-    return (
-      <>
-        {dateTemplate !== null && time.format(dateTemplate)}{' '}
-        {timeTemplate !== null && time.format(timeTemplate)}
-      </>
-    );
-  }
+
   return (
-    <span className={`time-wrapper ${className || ''}`}>
-      {dateTemplate !== null && (
-        <span className="date"> {time.format(dateTemplate)}</span>
-      )}
-      {timeTemplate !== null && (
-        <span className="time"> {time.format(timeTemplate)}</span>
-      )}
-    </span>
+    <Tooltip title={`${time.format(dateTemplate)} ${time.format(timeTemplate)}`}>
+      <span className={className}>{text}</span>
+    </Tooltip>
   );
 };
 
