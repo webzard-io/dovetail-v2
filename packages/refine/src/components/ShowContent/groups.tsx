d45eda1a3b90421d4ae9c2fcf72ed74bd6fca7bf
Bowen Tan <bowen.tan@smartx.com>
2024-03-06 11:45:03 +0800

    add SecretDataValue

--- packages/refine/src/components/ShowContent/groups.tsx
+++ packages/refine/src/components/ShowContent/groups.tsx
@@ -1,7 +1,14 @@
 import { i18n as I18nType } from 'i18next';
 import React from 'react';
 import { PodContainersTable } from 'src/components/PodContainersTable';
-import { ResourceModel, WorkloadBaseModel, ServiceModel, JobModel, CronJobModel, PodModel } from 'src/models';
+import {
+  ResourceModel,
+  WorkloadBaseModel,
+  ServiceModel,
+  JobModel,
+  CronJobModel,
+  PodModel,
+} from 'src/models';
 import {
   ShowField,
   ShowArea,
@@ -14,11 +21,20 @@ import {
   ConditionsField,
   ServicePodsField,
   JobsField,
+  SecretDataField,
 } from './fields';
 
 export const BasicGroup = <Model extends ResourceModel>(
   i18n: I18nType,
-  { upAreas = [], downAreas = [], basicFields = [] }: { upAreas?: ShowArea<Model>[]; downAreas?: ShowArea<Model>[]; basicFields?: ShowField<Model>[] } = {}
+  {
+    upAreas = [],
+    downAreas = [],
+    basicFields = [],
+  }: {
+    upAreas?: ShowArea<Model>[];
+    downAreas?: ShowArea<Model>[];
+    basicFields?: ShowField<Model>[];
+  } = {}
 ): ShowGroup<Model> => ({
   title: i18n.t('dovetail.basic_info'),
   areas: [
@@ -29,55 +45,79 @@ export const BasicGroup = <Model extends ResourceModel>(
         ...basicFields,
         LabelsField(i18n),
         AnnotationsField(i18n),
-        AgeField(i18n)
-      ]
+        AgeField(i18n),
+      ],
     },
-    ...downAreas
-  ]
+    ...downAreas,
+  ],
 });
 
 export const PodsGroup = <Model extends WorkloadBaseModel>(): ShowGroup<Model> => ({
   title: 'Pod',
-  areas: [{
-    fields: [PodsField()]
-  }]
+  areas: [
+    {
+      fields: [PodsField()],
+    },
+  ],
 });
 
-export const PodContainersGroup = <Model extends PodModel>(i18n: I18nType): ShowGroup<Model> => ({
+export const PodContainersGroup = <Model extends PodModel>(
+  i18n: I18nType
+): ShowGroup<Model> => ({
   title: i18n.t('dovetail.container'),
-  areas: [{
-    fields: [{
-      key: 'container',
-      path: [],
-      renderContent: (_, record) => {
-        return (
-          <PodContainersTable
-            containerStatuses={record.status?.containerStatuses || []}
-            initContainerStatuses={record.status?.initContainerStatuses || []}
-          />
-        );
-      },
-    }]
-  }]
+  areas: [
+    {
+      fields: [
+        {
+          key: 'container',
+          path: [],
+          renderContent: (_, record) => {
+            return (
+              <PodContainersTable
+                containerStatuses={record.status?.containerStatuses || []}
+                initContainerStatuses={record.status?.initContainerStatuses || []}
+              />
+            );
+          },
+        },
+      ],
+    },
+  ],
 });
 
 export const ServicePodsGroup = <Model extends ServiceModel>(): ShowGroup<Model> => ({
   title: 'Pod',
-  areas: [{
-    fields: [ServicePodsField()]
-  }]
+  areas: [
+    {
+      fields: [ServicePodsField()],
+    },
+  ],
 });
 
-export const ConditionsGroup = <Model extends ResourceModel>(i18n: I18nType): ShowGroup<Model> => ({
+export const ConditionsGroup = <Model extends ResourceModel>(
+  i18n: I18nType
+): ShowGroup<Model> => ({
   title: i18n.t('dovetail.condition'),
-  areas: [{
-    fields: [ConditionsField()]
-  }]
+  areas: [
+    {
+      fields: [ConditionsField()],
+    },
+  ],
+});
+
+export const SecretDataGroup = <Model extends ResourceModel>(): ShowGroup<Model> => ({
+  areas: [
+    {
+      fields: [SecretDataField()],
+    },
+  ],
 });
 
 export const JobsGroup = <Model extends JobModel | CronJobModel>(): ShowGroup<Model> => ({
   title: 'Job',
-  areas: [{
-    fields: [JobsField()]
-  }]
+  areas: [
+    {
+      fields: [JobsField()],
+    },
+  ],
 });
