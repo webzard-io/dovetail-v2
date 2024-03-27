c82ebc70e799cc40a9abbff033d964c231828a95
Bowen Tan <bowen.tan@smartx.com>
2024-03-27 11:51:55 +0800

    RefineForm support render custom field
    and fix some problems

--- packages/refine/src/components/Form/RefineFormContent.tsx
+++ packages/refine/src/components/Form/RefineFormContent.tsx
@@ -1,20 +1,24 @@
 import { Fields, Form, Space } from '@cloudtower/eagle';
 import { css } from '@linaria/core';
+import { UseFormReturnType } from '@refinedev/react-hook-form';
 import React from 'react';
-import { Control, Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 import { ResourceModel } from '../../models';
 import { ResourceConfig } from '../../types';
 import { FormErrorAlert } from '../FormErrorAlert';
 
 type Props<Model extends ResourceModel> = {
   config?: ResourceConfig<Model>;
-  control: Control;
+  formResult: UseFormReturnType;
   errorMsg?: string;
   action?: 'create' | 'edit';
 };
 
 export const RefineFormContent = <Model extends ResourceModel>(props: Props<Model>) => {
-  const { config, control, action, errorMsg } = props;
+  const { config, formResult, action, errorMsg } = props;
+  const { control, watch, getValues } = formResult;
+
+  watch(); // watching every fields in the form
 
   const fields = config?.formConfig?.fields?.map(c => {
     return (
@@ -23,16 +27,18 @@ export const RefineFormContent = <Model extends ResourceModel>(props: Props<Mode
         control={control}
         name={c.path.join('.')}
         rules={{
-          validate(value, formValues) {
+          validate(value) {
+            const formValue = getValues();
             if (!c.validators || c.validators.length === 0) return true;
             for (const func of c.validators) {
-              const { isValid, errorMsg } = func(value, formValues);
+              const { isValid, errorMsg } = func(value, formValue);
               if (!isValid) return errorMsg;
             }
             return true;
           },
         }}
         render={({ field: { onChange, onBlur, value, name }, fieldState }) => {
+          const formValue = getValues();
           let ele = (
             <Fields.String
               input={{ value, onChange, onBlur, name, onFocus: () => null }}
@@ -52,6 +58,10 @@ export const RefineFormContent = <Model extends ResourceModel>(props: Props<Mode
           // editing name is not allowed
           if (action === 'edit' && c.key === 'name') {
             ele = <div>{value}</div>;
+          }
+
+          if (c?.render) {
+            ele = c.render(value, onChange, formValue, onBlur);
           }
 
           return (
