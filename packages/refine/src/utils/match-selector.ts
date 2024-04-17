3b4a0227df0a345b1ac49249b98ee9e6c3ec3f16
MrWindlike <MrWindlike@gmail.com>
2024-04-17 14:15:49 +0800

    fix: use the namespace to filter the sub pods

--- packages/refine/src/utils/match-selector.ts
+++ packages/refine/src/utils/match-selector.ts
@@ -1,17 +1,20 @@
 import { LabelSelector } from 'kubernetes-types/meta/v1';
 import { PodModel } from '../models/pod-model';
 
-export function matchSelector(pod: PodModel, selector: LabelSelector): boolean {
+export function matchSelector(pod: PodModel, selector?: LabelSelector, namespace = 'default'): boolean {
   let match = true;
   // TODO: support complete selector match strategy
   // https://github.com/rancher/dashboard/blob/master/shell/utils/selector.js#L166
-  for (const key in selector.matchLabels) {
-    if (
-      !pod.metadata?.labels?.[key] ||
-      pod.metadata.labels?.[key] !== selector.matchLabels[key]
-    ) {
-      match = false;
+  if (selector) {
+    for (const key in selector.matchLabels) {
+      if (
+        !pod.metadata?.labels?.[key] ||
+        pod.metadata.labels?.[key] !== selector.matchLabels[key]
+      ) {
+        match = false;
+      }
     }
   }
-  return match;
+
+  return match && pod.metadata.namespace === namespace;
 }
