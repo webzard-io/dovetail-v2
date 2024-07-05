b4bb1005665fa1d1b8cca446e90d28cd01b3699b
Bowen Tan <bowen.tan@smartx.com>
2024-07-05 11:00:11 +0800

    export model plugin

--- packages/refine/src/plugins/index.ts
+++ packages/refine/src/plugins/index.ts
@@ -1,5 +1,7 @@
 import { IProviderPlugin } from 'k8s-api-provider';
 import { modelPlugin } from './model-plugin';
 import { relationPlugin } from './relation-plugin';
+export * from './model-plugin';
+export * from './relation-plugin';
 
 export const ProviderPlugins = [relationPlugin, modelPlugin] as IProviderPlugin[];
