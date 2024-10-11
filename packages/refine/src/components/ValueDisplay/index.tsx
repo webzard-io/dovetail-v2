054d4185a650ab2891ac8734d6eab058fbb876eb
MrWindlike <MrWindlike@gmail.com>
2024-10-11 14:44:33 +0800

    feat: support chagne the ui form to yaml editor

--- packages/refine/src/components/ValueDisplay/index.tsx
+++ packages/refine/src/components/ValueDisplay/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const EMPTY_VALUES: unknown[] = [undefined, null, '', '-'];
 
 const EmptyStyle = css`
-  && {
+  &.empty-text {
     color: rgba(0,21,64,.3);
   }
 `;
@@ -29,7 +29,7 @@ export function ValueDisplay(props: ValueDisplayProps) {
   const { value, useOverflow = true, className, style } = props;
 
   return EMPTY_VALUES.includes(value) ? (
-    <div className={cx(EmptyStyle, className)} style={style}>-</div>
+    <div className={cx(EmptyStyle, 'empty-text', className)} style={style}>-</div>
   ) : (
     <div
       style={style}
