44d7f1f35f78a97b215a50b510b8dc068924b066
bowen tan <bowentan@bowendeMacBook-Pro.local>
2024-10-24 16:15:00 +0800

    fix: fix custom dropdown dont close bug

--- packages/refine/src/components/DropdownMenuItems/EditLabelDropdownMenuItem.tsx
+++ packages/refine/src/components/DropdownMenuItems/EditLabelDropdownMenuItem.tsx
@@ -1,5 +1,6 @@
 import { Icon, Menu, usePushModal } from '@cloudtower/eagle';
 import { EditPen16BlueIcon } from '@cloudtower/icons-react';
+import { MenuItemProps } from 'antd/lib/menu/MenuItem';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ResourceModel } from '../../models';
@@ -11,7 +12,7 @@ type EditMenuItemProps<Model extends ResourceModel> = {
     submit: () => Promise<unknown> | undefined;
   } | null>;
   resourceModel: Model;
-};
+} & MenuItemProps;
 
 export function EditLabelDropdownMenuItem<Model extends ResourceModel>(
   props: EditMenuItemProps<Model>
@@ -23,18 +24,13 @@ export function EditLabelDropdownMenuItem<Model extends ResourceModel>(
     <Menu.Item
       {...props}
       className="ant-dropdown-menu-item"
-      onClick={() => {
+      onClick={(e) => {
         const modalProps = {
           formRef,
           title: t('dovetail.edit_label'),
           fullscreen: true,
           renderContent() {
-            return (
-              <EditLabelForm
-                ref={formRef}
-                resourceModel={resourceModel}
-              />
-            );
+            return <EditLabelForm ref={formRef} resourceModel={resourceModel} />;
           },
         };
 
@@ -42,6 +38,8 @@ export function EditLabelDropdownMenuItem<Model extends ResourceModel>(
           component: EditFieldModal,
           props: modalProps,
         });
+
+        props.onClick?.(e);
       }}
     >
       <Icon src={EditPen16BlueIcon}>{t('dovetail.edit_label')}</Icon>
