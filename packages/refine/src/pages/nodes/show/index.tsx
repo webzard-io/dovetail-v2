4d1db72aa1b39ec0aba4b5d5c44d5e52eba195cc
bowen tan <bowentan@bowendeMacBook-Pro.local>
2024-10-30 17:16:09 +0800

    feat: WorkloadPodsTable add loading

--- packages/refine/src/pages/nodes/show/index.tsx
+++ packages/refine/src/pages/nodes/show/index.tsx
@@ -2,6 +2,7 @@ import { IResourceComponentsProps } from '@refinedev/core';
 import { Unstructured } from 'k8s-api-provider';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { WorkloadPodsTable } from 'src/components';
 import K8sDropdown from 'src/components/Dropdowns/K8sDropdown';
 import { PageShow } from 'src/components/PageShow';
 import { BasicGroup, ConditionsGroup, NodeTaintsGroup } from 'src/components/ShowContent';
@@ -24,6 +25,35 @@ export const NodeShow: React.FC<IResourceComponentsProps> = () => {
               }),
               NodeTaintsGroup(i18n),
               ConditionsGroup(i18n),
+            ],
+          },
+          {
+            key: 'pods',
+            title: 'Pod',
+            groups: [
+              {
+                areas: [
+                  {
+                    fields: [
+                      {
+                        key: 'pods',
+                        path: [],
+                        renderContent: (_, record) => {
+                          return (
+                            <div style={{ padding: '0 24px', height: '100%' }}>
+                              <WorkloadPodsTable
+                                filter={pod => pod.spec?.nodeName === record.name}
+                                namespace={record.metadata.namespace}
+                                hideToolbar
+                              />
+                            </div>
+                          );
+                        },
+                      },
+                    ],
+                  },
+                ],
+              },
             ],
           },
         ],
