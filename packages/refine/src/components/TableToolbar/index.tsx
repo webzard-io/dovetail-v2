8e478aaa21126d39cb10068b183094cc32c8f9d3
MrWindlike <MrWindlike@gmail.com>
2025-06-24 14:32:26 +0800

    fix: update description type to React.ReactNode

--- packages/refine/src/components/TableToolbar/index.tsx
+++ packages/refine/src/components/TableToolbar/index.tsx
@@ -8,7 +8,7 @@ import { DeleteManyButton } from '../DeleteManyButton';
 
 type Props = {
   title?: string;
-  description?: string;
+  description?: React.ReactNode;
   selectedKeys: string[];
   hideCreate?: boolean;
 };
@@ -28,19 +28,23 @@ const DescriptionStyle = css`
   margin-top: 4px;
 `;
 
-export const TableToolBar: React.FC<Props> = ({ title, description, selectedKeys, hideCreate }) => {
+export const TableToolBar: React.FC<Props> = ({
+  title,
+  description,
+  selectedKeys,
+  hideCreate,
+}) => {
   const { resource } = useResource();
 
   return (
     <div className={cx(ToolbarWrapperStyle, 'table-toolbar')}>
       <Space className={ToolbarStyle}>
-        <span className={cx(Typo.Display.d2_regular_title, TitleStyle)}>{title || resource?.meta?.kind}</span>
+        <span className={cx(Typo.Display.d2_regular_title, TitleStyle)}>
+          {title || resource?.meta?.kind}
+        </span>
         <Space>
           {selectedKeys.length > 0 ? <DeleteManyButton ids={selectedKeys} /> : undefined}
-          <CanAccess
-            resource={resource?.name}
-            action={AccessControlAuth.Create}
-          >
+          <CanAccess resource={resource?.name} action={AccessControlAuth.Create}>
             {!hideCreate ? <CreateButton label={title} /> : null}
           </CanAccess>
         </Space>
