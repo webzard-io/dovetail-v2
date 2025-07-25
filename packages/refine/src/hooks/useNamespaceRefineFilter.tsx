8cab8e9fcc336ce8addca837dfedd9c625f8f03b
bowen tan <bowentan@bowendeMacBook-Pro.local>
2025-07-25 14:55:04 +0800

    fix: dont return filters when namespace is _all

--- packages/refine/src/hooks/useNamespaceRefineFilter.tsx
+++ packages/refine/src/hooks/useNamespaceRefineFilter.tsx
@@ -4,18 +4,28 @@ import { useNamespacesFilter, ALL_NS } from 'src/components/NamespacesFilter';
 
 function useNamespaceRefineFilter() {
   const { value: nsFilters = [] } = useNamespacesFilter();
-  const filters = useMemo(() => ({
-    permanent: [
-      {
-        operator: 'or',
-        value: nsFilters.filter(filter => filter !== ALL_NS).map(filter => ({
-          field: 'metadata.namespace',
-          operator: 'eq',
-          value: filter,
-        }))
-      }
-    ] as CrudFilters,
-  }), [nsFilters]);
+  const filters = useMemo(() => {
+    const filters = nsFilters.filter(filter => filter !== ALL_NS);
+    if (filters.length === 0) {
+      return {
+        permanent: [],
+      };
+    }
+
+    return {
+      permanent: [
+        {
+          operator: 'or',
+          value: filters
+            .map(filter => ({
+              field: 'metadata.namespace',
+              operator: 'eq',
+              value: filter,
+            })),
+        },
+      ] as CrudFilters,
+    };
+  }, [nsFilters]);
 
   return filters;
 }
