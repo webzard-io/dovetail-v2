232caa8bdf50709d8e0186df3b4ebd4af912e553
MrWindlike <MrWindlike@gmail.com>
2025-08-29 02:32:36 +0800

    feat: add MatchLabelSelector and PortsConfigForm

--- packages/refine/src/utils/validation.ts
+++ packages/refine/src/utils/validation.ts
@@ -1,3 +1,8 @@
+import { i18n as I18n } from 'i18next';
+
+const Rfc1123NameRegExp = /(^[a-z0-9]$)|(^[a-z0-9][a-z0-9-]*[a-z0-9]$)/;
+const Rfc1035NameRegExp = /(^[a-z]$)|(^[a-z][a-z0-9\-]*[a-z0-9]$)/;
+
 export function validateDnsSubdomain(subdomain: string): {
   isValid: boolean;
   errorMessage?: string;
@@ -13,6 +18,101 @@ export function validateDnsSubdomain(subdomain: string): {
   }
 
   return { isValid: true };
+}
+
+interface ValidateResourceNameOptions {
+  v: string;
+  allNames: string[];
+  i18n: I18n;
+  emptyText?: string;
+  duplicatedText?: string;
+  formatErrorText: string;
+  regex: RegExp;
+}
+
+export function validateResourceName({
+  v,
+  allNames,
+  i18n,
+  emptyText,
+  duplicatedText,
+  formatErrorText,
+  regex,
+}: ValidateResourceNameOptions) {
+  if (!v) {
+    return {
+      isValid: false,
+      errorMessage: emptyText || i18n.t('dovetail.required_field', {
+        label: i18n.t('dovetail.name'),
+      }),
+    };
+  }
+
+  if (v.length > 63) {
+    return {
+      isValid: false,
+      errorMessage: i18n.t('dovetail.length_limit', {
+        label: i18n.t('dovetail.name'),
+        minLength: 1,
+        maxLength: 63,
+      }),
+    };
+  }
+
+  if (!v.match(regex)) {
+    return {
+      isValid: false,
+      errorMessage: formatErrorText,
+    };
+  }
+
+  if (allNames.includes(v)) {
+    return {
+      isValid: false,
+      errorMessage: duplicatedText || i18n.t('dovetail.name_duplicated', {
+        name: v,
+      }),
+    };
+  }
+  return {
+    isValid: true,
+  };
+}
+
+export function validateRfc1123Name({
+  v,
+  allNames,
+  i18n,
+  emptyText,
+  duplicatedText,
+}: Omit<ValidateResourceNameOptions, 'regex' | 'formatErrorText'>) {
+  return validateResourceName({
+    v,
+    allNames,
+    i18n,
+    emptyText,
+    duplicatedText,
+    regex: Rfc1123NameRegExp,
+    formatErrorText: i18n.t('dovetail.rf1123_name_format_error'),
+  });
+}
+
+export function ValidateRfc1035Name({
+  v,
+  allNames,
+  i18n,
+  emptyText,
+  duplicatedText,
+}: Omit<ValidateResourceNameOptions, 'regex' | 'formatErrorText'>) {
+  return validateResourceName({
+    v,
+    allNames,
+    i18n,
+    emptyText,
+    duplicatedText,
+    regex: Rfc1035NameRegExp,
+    formatErrorText: i18n.t('dovetail.rf1035_name_format_error'),
+  });
 }
 
 export function validateLabelKey(key: string): {
@@ -64,6 +164,28 @@ export function validateLabelValue(
 
   if (!labelValueRegex.test(value)) {
     return { isValid: false };
+  }
+
+  return { isValid: true };
+}
+
+export function validatePort(port: string | number, isOptional: boolean, i18n: I18n): {
+  isValid: boolean;
+  errorMessage?: string;
+} {
+
+  if (port === '' && !isOptional) {
+    return {
+      isValid: false, errorMessage: i18n.t('dovetail.required_field', {
+        label: i18n.t('dovetail.port'),
+      })
+    };
+  }
+
+  const portNumber = Number(port);
+
+  if (portNumber < 1 || portNumber > 65535) {
+    return { isValid: false, errorMessage: i18n.t('dovetail.input_correct_port') };
   }
 
   return { isValid: true };
