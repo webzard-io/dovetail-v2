63f1bce7c5de02a4dc46b456b54d77b8f049997d
MrWindlike <MrWindlike@gmail.com>
2026-05-11 15:59:53 +0800

    refactor: deduplicate model methods and enhance ComponentContext
    - Extract shared fetchRestarts() to WorkloadBaseModel base class
    - Move getControllerRevisions() and getRevision() to WorkloadModel,
      using this.kind for polymorphic filtering instead of hardcoded strings
    - Remove duplicate methods from DaemonSetModel and StatefulSetModel
    - Add ComponentContextProvider wrapper and ComponentContextValue type
    - Export ComponentContextProvider and type from contexts barrel

--- packages/refine/src/contexts/index.ts
+++ packages/refine/src/contexts/index.ts
@@ -1,3 +1,4 @@
 export { default as ComponentContext } from './component';
+export { ComponentContextProvider, type ComponentContextValue } from './component';
 export { default as GlobalStoreContext } from './global-store';
 export { default as ConfigsContext } from './configs';
